8af1025e489e3774963ebab90f77480a53272425
devjohnpark <devjohnpark@gmail.com>
2022-08-18 16:57:49 +0900

    table view as code

---
+++ TableView/TableView/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by Junseo Park on 2022/08/18.
+//
+
+import UIKit
+
+// MARK:  - TableView as code
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    // MARK: - TableView
+    
+    private let tableView: UITableView = {
+       let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        // Similar View - ViewModel arhitecture
+        var configure = cell.defaultContentConfiguration()
+        configure.text = "alarm  tableView: \(indexPath.row)"
+        configure.image = UIImage(systemName: "bell")
+         
+        cell.contentConfiguration = configure
+        
+        return cell
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
+}
+
